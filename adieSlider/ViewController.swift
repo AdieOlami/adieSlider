57f1a970cc118b7503071700cbb391e5cd5ac603
Adie Olalekan <ugbeadie@gmail.com>
2019-02-08 19:27:30 +0100

    Initial Commit

---
+++ adieSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  adieSlider
+//
+//  Created by Olar's Mac on 2/8/19.
+//  Copyright © 2019 Adie Olami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
