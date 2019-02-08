b04a68b92f747299699c7d5ee4dcc377b1b4105e
Adie Olalekan <ugbeadie@gmail.com>
2019-02-08 19:34:00 +0100

    Base

---
+++ adieSlider/adieSlider.swift
@@ -0,0 +1,15 @@
+//
+//  adieSlider.swift
+//  adieSlider
+//
+//  Created by Olar's Mac on 2/8/19.
+//  Copyright © 2019 Adie Olami. All rights reserved.
+//
+
+import UIKit
+
+public protocol FunmiDataSource: class {
+    
+    func slideBegin(_ action: String, atIndex index: Int)
+    
+}
